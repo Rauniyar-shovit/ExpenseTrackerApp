6b35cd327f025fa12a119029b0b0185be68b56d8
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2025-04-26 23:18:52 +1000

    added profile tabs and logout functionality

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,195 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import Header from "@/components/Header";
+import Typo from "@/components/Typo";
+import { useAuth } from "@/contexts/authContext";
+import { Image } from "expo-image";
+import { getProfileImage } from "@/services/imageServices";
+import { accountOptionType } from "@/types";
+import * as Icons from "phosphor-react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { useRouter } from "expo-router";
+import LogoutModal from "@/components/LogoutModal";
+import { signOut } from "firebase/auth";
+import { auth } from "@/config/firebase";
 
 const Profile = () => {
+  const [logoutModalVisible, setLogoutModalVisible] = useState(false);
+  const { user } = useAuth();
+  const router = useRouter();
+  const accountOptions: accountOptionType[] = [
+    {
+      title: "Edit Profile",
+      icon: <Icons.User size={26} color={colors.white} weight="fill" />,
+      routeName: "/(modals)/profileModal",
+      bgColor: "#6366f1",
+    },
+    {
+      title: "Settings",
+      icon: <Icons.GearSix size={26} color={colors.white} weight="fill" />,
+      // routeName: "/(modals)/profileModal",
+      bgColor: "#059669",
+    },
+    {
+      title: "Privacy Policy",
+      icon: <Icons.User size={26} color={colors.white} weight="fill" />,
+      // routeName: "/(modals)/profileModal",
+      bgColor: colors.neutral600,
+    },
+    {
+      title: "Logout",
+      icon: <Icons.User size={26} color={colors.white} weight="fill" />,
+      // routeName: "/(modals)/profileModal",
+      bgColor: "#e11d48",
+    },
+  ];
+  const handlePress = (item: accountOptionType) => {
+    if (item.title == "Logout") {
+      setLogoutModalVisible(true);
+    }
+  };
+
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
   return (
     <ScreenWrapper>
-      <Text>Profile</Text>
+      <View style={styles.container}>
+        <Header title={"Profile"} style={{ marginVertical: spacingY._10 }} />
+
+        {/* user Info */}
+        <View style={styles.userInfo}>
+          {/* avatar */}
+          <View></View>
+          {/* user image */}
+          <Image
+            style={styles.avatar}
+            source={getProfileImage(user?.image)}
+            contentFit="cover"
+            transition={100}
+          />
+
+          {/* name & email */}
+          <View style={styles.nameContainer}>
+            <Typo size={24} fontWeight={"600"} color={colors.neutral100}>
+              {user?.name}
+            </Typo>
+            <Typo size={15} color={colors.neutral400}>
+              {user?.email}
+            </Typo>
+          </View>
+        </View>
+
+        {/* account options */}
+        <View style={styles.accountOptions}>
+          {accountOptions.map((item, index) => {
+            return (
+              <Animated.View
+                key={index.toString()}
+                entering={FadeInDown.delay(index * 50)
+                  .springify()
+                  .damping(14)}
+                style={styles.listItem}
+              >
+                <TouchableOpacity
+                  style={styles.flexRow}
+                  onPress={() => handlePress(item)}
+                >
+                  {/* icon */}
+                  <View
+                    style={[
+                      styles.listIcon,
+                      { backgroundColor: item?.bgColor },
+                    ]}
+                  >
+                    {item.icon && item.icon}
+                  </View>
+                  <Typo size={16} style={{ flex: 1 }} fontWeight={"500"}>
+                    {item.title}
+                  </Typo>
+                  <Icons.CaretRight
+                    size={verticalScale(20)}
+                    weight="bold"
+                    color={colors.white}
+                  />
+                </TouchableOpacity>
+              </Animated.View>
+            );
+          })}
+        </View>
+      </View>
+      <LogoutModal
+        setModalVisible={setLogoutModalVisible}
+        modalVisible={logoutModalVisible}
+        handleLogout={handleLogout}
+      />
     </ScreenWrapper>
   );
 };
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: spacingX._20,
+  },
+  userInfo: {
+    marginTop: verticalScale(30),
+    alignItems: "center",
+    gap: spacingY._15,
+  },
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  avatar: {
+    alignSelf: "center",
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: 200,
+    // overflow: "hidden",
+    // position: "relative", // optional
+  },
+  editIcon: {
+    position: "absolute",
+    bottom: 5,
+    right: 8,
+    borderRadius: 50,
+    backgroundColor: colors.neutral100,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: 5,
+  },
+  nameContainer: {
+    gap: verticalScale(4),
+    alignItems: "center",
+  },
+  listIcon: {
+    height: verticalScale(44),
+    width: verticalScale(44),
+    backgroundColor: colors.neutral500,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: radius._15,
+    borderCurve: "continuous",
+  },
+  listItem: {
+    marginBottom: verticalScale(17),
+  },
+  accountOptions: {
+    marginTop: spacingY._35,
+  },
+  flexRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: spacingX._10,
+  },
+});
---
+++ components/LogoutModal.tsx
@@ -0,0 +1,107 @@
+import { Modal, StyleSheet, View } from "react-native";
+import React, { Dispatch, useState } from "react";
+import Typo from "./Typo";
+import { colors } from "@/constants/theme";
+import { scale, verticalScale } from "@/utils/styling";
+import Button from "./Button";
+
+type LogoutModalProps = {
+  modalVisible: boolean;
+  setModalVisible: Dispatch<React.SetStateAction<boolean>>;
+  handleLogout: () => void;
+};
+
+const LogoutModal = ({
+  modalVisible,
+  setModalVisible,
+  handleLogout,
+}: LogoutModalProps) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        setModalVisible(!modalVisible);
+      }}
+    >
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <View style={styles.modalView}>
+          <Typo
+            size={24}
+            fontWeight={"600"}
+            color={colors.white}
+            style={{
+              marginBottom: verticalScale(20),
+            }}
+          >
+            Log Out?
+          </Typo>
+          <Typo
+            size={18}
+            fontWeight={"400"}
+            color={colors.white}
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            Are you sure you want to logout ?
+          </Typo>
+
+          <View style={styles.buttonsContainer}>
+            <Button
+              style={{ ...styles.button, backgroundColor: colors.white }}
+              onPress={() => setModalVisible(false)}
+            >
+              <Typo color={colors.black} size={15}>
+                Cancel
+              </Typo>
+            </Button>
+            <Button style={styles.button} onPress={handleLogout}>
+              <Typo color={colors.black} size={15}>
+                Logout
+              </Typo>
+            </Button>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default LogoutModal;
+
+const styles = StyleSheet.create({
+  buttonsContainer: {
+    marginTop: verticalScale(30),
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: scale(10),
+  },
+  button: {
+    paddingHorizontal: scale(30),
+    height: verticalScale(50),
+  },
+
+  cancelButton: {
+    backgroundColor: colors.neutral700,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: colors.neutral800,
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
